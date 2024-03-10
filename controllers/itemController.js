5250a713dab05f01d52cb11890e7b5b1c267d417
G-Alessandro <galloalessandro77@gmail.com>
2024-03-10 23:22:52 +0100

    Fixed category duplication in the sidebar

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,9 +3,8 @@ const asyncHandler = require('express-async-handler');
 const Item = require('../models/item');
 
 exports.item_category_list = asyncHandler(async (req, res, next) => {
-  const allCategories = await Item.find({}, 'category')
+  const allCategories = await Item.distinct('category')
     .sort({ category: 1 })
     .exec();
-
-  res.render('index', { title: 'Category List', item_category_list: allCategories });
+  res.render('layout', { item_category_list: allCategories });
 });
