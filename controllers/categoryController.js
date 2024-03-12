1c3a07b3cdb3515e4bc3babd55b1d318872cdded
G-Alessandro <galloalessandro77@gmail.com>
2024-03-12 18:08:52 +0100

    Added route for each category and category_items in categoryController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -6,15 +6,22 @@ exports.categories_list = asyncHandler(async (req, res, next) => {
   const allCategories = await Item.distinct('category')
     .sort({ category: 1 })
     .exec();
-  console.log('allCategories', allCategories);
   res.render('layout', { categories_list: allCategories });
 });
 
 exports.all_items = asyncHandler(async (req, res, next) => {
-  const allCategories = await Item.distinct('category')
-    .sort({ category: 1 })
-    .exec();
-  const allItems = await Item.find({}, 'category').exec();
+  const [allCategories, allItems] = await Promise.all([
+    Item.distinct('category').sort({ category: 1 }).exec(),
+    Item.find({}, 'category').sort({ category: 1 }).exec(),
+  ]);
   console.log('allItems', allItems);
-  res.render('items_container', { all_items: allItems, categories_list: allCategories });
+  res.render('items_container', { categories_list: allCategories, all_items: allItems });
+});
+
+exports.category_items = asyncHandler(async (req, res, next) => {
+  const [allCategories, categoryItems] = await Promise.all([
+    Item.distinct('category').sort({ category: 1 }).exec(),
+    Item.find({ category: req.params.categoryName }).exec(),
+  ]);
+  res.render('items_container', { categories_list: allCategories, all_items: categoryItems });
 });
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,6 @@ router.get('/', item_controller.categories_list);
 
 router.get('/all-categories', item_controller.all_items);
 
+router.get('/category/:categoryName', item_controller.category_items);
+
 module.exports = router;
